dfceb6f647a01e76b09245ef9db7a67f2109f162
Moazam Ali <moazamshahid555@gmail.com>
2022-05-10 01:19:33 +0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+(function ($){
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+}
+)(jQuery)
